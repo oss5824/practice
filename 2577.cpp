a9fd2f209e86047ab69980fd99cc13a9601e1fcc
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-01-09 00:21:52 +0900

    정답
    숫자의 개수

---
+++ 2577.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int a, b, c, len = 0;
+	unsigned long long res, l_res;
+	cin >> a >> b >> c;
+	int arr[10] = { 0, };
+	res = a * b * c;
+	l_res = res;
+	while (l_res > 0)
+	{
+		l_res /= 10;
+		len++;
+	}
+	len--;
+	while (len >= 0)
+	{
+		unsigned long long l = pow(10, len);
+		int d = res / l;
+		arr[d]++;
+		res = res - d * l;
+		len--;
+	}
+	for (int i = 0; i < 10; i++)printf("%d\n", arr[i]);
+	return 0;
+}
