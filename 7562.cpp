aa8d82b22702495b2a9646102e72006df97dfd98
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-01-17 15:40:47 +0900

    정답
    나이트의 이동

--- 7562.cpp
+++ 7562.cpp
@@ -1,34 +1,33 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <queue>
-#include <cstring>
 #include <utility>
+#include <cstring>
 using namespace std;
-int arr[1001];
-int M[1001];
-int n, m, vmin = 1000000;
+int arr[301][301];
+int M[301][301];
+int n, l;
+int a, b, c, d;
+int mx[8] = { -1,1,2,2,-1,1,-2,-2 };
+int my[8] = { -2,-2,-1,1,2,2,-1,1 };
 void bfs()
 {
-	queue<int>q;
-	q.push(n);
+	queue<pair<int, int>>q;
+	q.push(make_pair(b, a));
+	arr[b][a] = 1;
 	while (!(q.empty()))
 	{
-		int pos = q.front();
+		int y = q.front().first, x = q.front().second;
 		q.pop();
-		arr[pos] = 1;
-		int move[3] = { 1,-1,pos };
-		for (int k = 0; k < 3; k++)
+		if (d == y && c == x)break;
+		arr[y][x] = 1;
+		for (int k = 0; k < 8; k++)
 		{
-			if (pos + move[k] < 0 || pos + move[k] >1000)continue;
-			if (arr[pos + move[k]] == 0)
+			if (y + my[k] < 0 || y + my[k] >= l || x + mx[k] < 0 || x + mx[k] >= l)continue;
+			if (arr[y + my[k]][x + mx[k]] == 0)
 			{
-				arr[pos + move[k]] = 1;
-				M[pos + move[k]] = M[pos] + 1;
-				printf("pos : %d, pos+move : %d, M[pos+move] : %d\n", pos, pos + move[k], 
-					M[pos + move[k]]);
-				if (pos + move[k] == m)vmin = min(vmin, M[pos + move[k]]);
-				q.push(pos + move[k]);
+				arr[y + my[k]][x + mx[k]] = 1;
+				M[y + my[k]][x + mx[k]] = M[y][x] + 1;
+				q.push(make_pair(y + my[k], x + mx[k]));
 			}
 		}
 	}
@@ -37,10 +36,16 @@ int main()
 {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
-	cin >> n >> m;
-	memset(arr, 0, sizeof(arr));
-	memset(M, 0, sizeof(M));
-	bfs();
-	printf("%d", vmin);
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> l;
+		cin >> b >> a;
+		cin >> d >> c;
+		memset(arr, 0, sizeof(arr));
+		memset(M, 0, sizeof(M));
+		bfs();
+		printf("%d\n", M[d][c]);
+	}
 	return 0;
 }
