d6c4bdeb17b1a0a41c1f745608a0d23f6d80265a
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-01-19 17:27:59 +0900

    정답
    플로이드-플로이드와셜알고리즘으로 풀어야되는데 벨만포드로풀었음

--- 11404.cpp
+++ 11404.cpp
@@ -1,46 +1,57 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <queue>
-#include <cstring>
 #include <utility>
+#include <algorithm>
+#define INF 1e9
 using namespace std;
-int arr[1001];
-int M[1001];
-int n, m, vmin = 1000000;
-void bfs()
+vector<pair<int, long long>>* v;
+long long** dist;
+int V, E;
+bool bellman(int start)
 {
-	queue<int>q;
-	q.push(n);
-	while (!(q.empty()))
+	dist[start][start] = 0;
+	for (int i = 1; i <= V; i++)
 	{
-		int pos = q.front();
-		q.pop();
-		arr[pos] = 1;
-		int move[3] = { 1,-1,pos };
-		for (int k = 0; k < 3; k++)
+		for (int j = 1; j <= V; j++)
 		{
-			if (pos + move[k] < 0 || pos + move[k] >1000)continue;
-			if (arr[pos + move[k]] == 0)
+			if (dist[start][j] == INF)continue;
+			int len = v[j].size();
+			long long cur_distance = dist[start][j];
+			for (int k = 0; k < len; k++)
 			{
-				arr[pos + move[k]] = 1;
-				M[pos + move[k]] = M[pos] + 1;
-				printf("pos : %d, pos+move : %d, M[pos+move] : %d\n", pos, pos + move[k], 
-					M[pos + move[k]]);
-				if (pos + move[k] == m)vmin = min(vmin, M[pos + move[k]]);
-				q.push(pos + move[k]);
+				int next = v[j][k].first;
+				long long next_distance = v[j][k].second;
+				if (dist[start][next] > cur_distance + next_distance)
+					dist[start][next] = cur_distance + next_distance;
 			}
 		}
 	}
+	return true;
 }
 int main()
 {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
-	cin >> n >> m;
-	memset(arr, 0, sizeof(arr));
-	memset(M, 0, sizeof(M));
-	bfs();
-	printf("%d", vmin);
+	cin >> V >> E;
+	v = new vector<pair<int, long long>>[V + 1];
+	dist = new long long*[V + 1];
+	for (int i = 0; i <= V; i++)dist[i] = new long long[V + 1];
+	for (int i = 0; i <= V; i++)for (int j = 0; j <= V; j++)dist[i][j] = INF;
+	for (int i = 0; i < E; i++)
+	{
+		int a, b, value;
+		cin >> a >> b >> value;
+		v[a].push_back(make_pair(b, value));
+	}
+	for (int i = 1; i <= V; i++) bellman(i);
+	for (int i = 1; i <= V; i++)
+	{
+		for (int j = 1; j <= V; j++)
+		{
+			if (dist[i][j] == INF)printf("0 ");
+			else printf("%lld ", dist[i][j]);
+		}
+		printf("\n");
+	}
 	return 0;
 }
