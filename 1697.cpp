c5fedf3294c4822effd28cd0b1f28aa37ca469e9
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-01-19 14:32:37 +0900

    정답
    숨바꼭질 -문제 제대로이해못해서 오래걸림(마이너스 방향으로 순간이동하는 걸 포함해버림, 플러스 방향만 순간이동 가능)

--- 1697.cpp
+++ 1697.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <queue>
 #include <cstring>
-#include <utility>
 using namespace std;
-int arr[1001];
-int M[1001];
-int n, m, vmin = 1000000;
+int arr[100001];
+int M[100001];
+int n, l, vmin = 0;
 void bfs()
 {
 	queue<int>q;
 	q.push(n);
+	bool end = false;
 	while (!(q.empty()))
 	{
 		int pos = q.front();
 		q.pop();
+		if (pos == l)break;
 		arr[pos] = 1;
 		int move[3] = { 1,-1,pos };
 		for (int k = 0; k < 3; k++)
 		{
-			if (pos + move[k] < 0 || pos + move[k] >1000)continue;
+			if (pos + move[k] < 0 || pos + move[k] >100000)continue;
 			if (arr[pos + move[k]] == 0)
 			{
 				arr[pos + move[k]] = 1;
 				M[pos + move[k]] = M[pos] + 1;
-				printf("pos : %d, pos+move : %d, M[pos+move] : %d\n", pos, pos + move[k], 
-					M[pos + move[k]]);
-				if (pos + move[k] == m)vmin = min(vmin, M[pos + move[k]]);
 				q.push(pos + move[k]);
 			}
 		}
@@ -37,10 +33,10 @@ int main()
 {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
-	cin >> n >> m;
+	cin >> n >> l;
 	memset(arr, 0, sizeof(arr));
 	memset(M, 0, sizeof(M));
 	bfs();
-	printf("%d", vmin);
+	printf("%d", M[l]);
 	return 0;
 }
