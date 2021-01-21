18a1a3b29f5952ef31a1029c28e8964f16001cd9
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-01-21 23:25:24 +0900

    정답
    두용액-투포인터

--- 2470.cpp
+++ 2470.cpp
@@ -1,46 +1,37 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <queue>
-#include <cstring>
-#include <utility>
+#include <cmath>
 using namespace std;
-int arr[1001];
-int M[1001];
-int n, m, vmin = 1000000;
-void bfs()
-{
-	queue<int>q;
-	q.push(n);
-	while (!(q.empty()))
-	{
-		int pos = q.front();
-		q.pop();
-		arr[pos] = 1;
-		int move[3] = { 1,-1,pos };
-		for (int k = 0; k < 3; k++)
-		{
-			if (pos + move[k] < 0 || pos + move[k] >1000)continue;
-			if (arr[pos + move[k]] == 0)
-			{
-				arr[pos + move[k]] = 1;
-				M[pos + move[k]] = M[pos] + 1;
-				printf("pos : %d, pos+move : %d, M[pos+move] : %d\n", pos, pos + move[k], 
-					M[pos + move[k]]);
-				if (pos + move[k] == m)vmin = min(vmin, M[pos + move[k]]);
-				q.push(pos + move[k]);
-			}
-		}
-	}
-}
 int main()
 {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
-	cin >> n >> m;
-	memset(arr, 0, sizeof(arr));
-	memset(M, 0, sizeof(M));
-	bfs();
-	printf("%d", vmin);
+	int n;
+	cin >> n;
+	long long* arr = new long long[n];
+	for (int i = 0; i < n; i++)cin >> arr[i];
+	sort(arr, arr + n);
+	if (arr[0] > 0 && arr[1] > 0)printf("%lld %lld", arr[0], arr[1]);
+	else if (arr[n - 2] < 0 && arr[n - 1] < 0)printf("%lld %lld", arr[n - 2], arr[n - 1]);
+	else
+	{
+		int start = 0, end = n - 1;
+		long long min_value = 2000000000;
+		pair<long long, long long>p;
+		while (start < end)
+		{
+			long long value = arr[end] + arr[start];
+			if (min_value > (long long)abs(value))
+			{
+				min_value = (long long)abs(value);
+				p.first = arr[start];
+				p.second = arr[end];
+			}
+			if (value > 0)end--;
+			else if (value < 0)start++;
+			else break;
+		}
+		printf("%lld %lld", p.first, p.second);
+	}
 	return 0;
 }
