e933befc9631f87e3c55564a229286fc596307b3
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-02-01 02:48:45 +0900

    정답
    특정한 최단경로-다익스트라 시작될 때마다 배열 INF로 초기화 안시켜줘서 오래걸림

--- 1504.cpp
+++ 1504.cpp
@@ -1,46 +1,59 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <queue>
-#include <cstring>
 #include <utility>
+#include <algorithm>
+#define INF 1e9
 using namespace std;
-int arr[1001];
-int M[1001];
-int n, m, vmin = 1000000;
-void bfs()
+vector<pair<int, int>>* v;
+int* dist;
+int V, E;
+int shortest(int b, int e)
 {
-	queue<int>q;
-	q.push(n);
-	while (!(q.empty()))
+	fill(dist, dist + V + 1, INF);
+	priority_queue<pair<int, int>>pq;
+	pq.push(make_pair(0, b));
+	dist[b] = 0;
+	while (!(pq.empty()))
 	{
-		int pos = q.front();
-		q.pop();
-		arr[pos] = 1;
-		int move[3] = { 1,-1,pos };
-		for (int k = 0; k < 3; k++)
+		int cur_distance = -pq.top().first;
+		int cur = pq.top().second;
+		int len = v[cur].size();
+		pq.pop();
+		for (int i = 0; i < len; i++)
 		{
-			if (pos + move[k] < 0 || pos + move[k] >1000)continue;
-			if (arr[pos + move[k]] == 0)
+			int next = v[cur][i].first;
+			int next_distance = v[cur][i].second;
+			if (dist[next] > cur_distance + next_distance)
 			{
-				arr[pos + move[k]] = 1;
-				M[pos + move[k]] = M[pos] + 1;
-				printf("pos : %d, pos+move : %d, M[pos+move] : %d\n", pos, pos + move[k], 
-					M[pos + move[k]]);
-				if (pos + move[k] == m)vmin = min(vmin, M[pos + move[k]]);
-				q.push(pos + move[k]);
+				dist[next] = cur_distance + next_distance;
+				pq.push(make_pair(-dist[next], next));
 			}
 		}
 	}
+	return dist[e];
 }
 int main()
 {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
-	cin >> n >> m;
-	memset(arr, 0, sizeof(arr));
-	memset(M, 0, sizeof(M));
-	bfs();
-	printf("%d", vmin);
+	int v1, v2;
+	cin >> V >> E;
+	v = new vector<pair<int, int>>[V + 1];
+	dist = new int[V + 1];
+	for (int i = 0; i < E; i++)
+	{
+		int a, b, value;
+		cin >> a >> b >> value;
+		v[a].push_back(make_pair(b, value));
+		v[b].push_back(make_pair(a, value));
+	}
+	cin >> v1 >> v2;
+	int startToV1 = shortest(1, v1), V1ToV2 = shortest(v1, v2), V2ToEnd = shortest(v2, V);
+	bool INF1 = (startToV1 == INF || V1ToV2 == INF || V2ToEnd == INF) ? true : false;
+	int startToV2 = shortest(1, v2), V2ToV1 = V1ToV2, V1ToEnd = shortest(v1, V);
+	bool INF2 = (startToV2 == INF || V2ToV1 == INF || V1ToEnd == INF)? true : false;
+	if (INF1 && INF2)printf("-1");
+	else printf("%d", min(startToV2 + V2ToV1 + V1ToEnd,
+		startToV1 + V1ToV2 + V2ToEnd));
 	return 0;
 }
