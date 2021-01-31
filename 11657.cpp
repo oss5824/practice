f2055b251f6a56dacd582f5097432ded40ff0adc
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-02-01 01:00:42 +0900

    정답
    타임머신-출발점의 값이 갱신되지 않은 지역이면 조건에서 예외를 걸어줘야하는데 해당 조건을 안 넣어줘서 틀렸었음

--- 11657.cpp
+++ 11657.cpp
@@ -1,46 +1,58 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <queue>
-#include <cstring>
 #include <utility>
+#include <algorithm>
+#define INF 1e9
 using namespace std;
-int arr[1001];
-int M[1001];
-int n, m, vmin = 1000000;
-void bfs()
+vector<pair<int, long long>>* v;
+long long* dist;
+int V, E;
+bool bellman()
 {
-	queue<int>q;
-	q.push(n);
-	while (!(q.empty()))
+	dist[1] = 0;
+	for (int i = 1; i <= V; i++)
 	{
-		int pos = q.front();
-		q.pop();
-		arr[pos] = 1;
-		int move[3] = { 1,-1,pos };
-		for (int k = 0; k < 3; k++)
+		for (int j = 1; j <= V; j++)
 		{
-			if (pos + move[k] < 0 || pos + move[k] >1000)continue;
-			if (arr[pos + move[k]] == 0)
+			if (dist[j] == INF)continue;
+			int len = v[j].size();
+			long long cur_distance = dist[j];
+			for (int k = 0; k < len; k++)
 			{
-				arr[pos + move[k]] = 1;
-				M[pos + move[k]] = M[pos] + 1;
-				printf("pos : %d, pos+move : %d, M[pos+move] : %d\n", pos, pos + move[k], 
-					M[pos + move[k]]);
-				if (pos + move[k] == m)vmin = min(vmin, M[pos + move[k]]);
-				q.push(pos + move[k]);
+				int next = v[j][k].first;
+				long long next_distance = v[j][k].second;
+				if (dist[next] > cur_distance + next_distance)
+				{
+					dist[next] = cur_distance + next_distance;
+					if (i == V)return false;
+				}
 			}
 		}
 	}
+	return true;
 }
 int main()
 {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
-	cin >> n >> m;
-	memset(arr, 0, sizeof(arr));
-	memset(M, 0, sizeof(M));
-	bfs();
-	printf("%d", vmin);
+	cin >> V >> E;
+	v = new vector<pair<int, long long>>[V + 1];
+	dist = new long long[V + 1];
+	fill(dist, dist + V + 1, INF);
+	for (int i = 0; i < E; i++)
+	{
+		int a, b, value;
+		cin >> a >> b >> value;
+		v[a].push_back(make_pair(b, value));
+	}
+	if (!bellman())printf("-1");
+	else
+	{
+		for (int i = 2; i <= V; i++)
+		{
+			if (dist[i] == INF)printf("-1\n");
+			else printf("%lld\n", dist[i]);
+		}
+	}
 	return 0;
 }
