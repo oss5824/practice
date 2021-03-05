0048dce0d3398acf3bf3c53ccd2440ab0af409a8
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-03-06 03:12:37 +0900

    .
    .

---
+++ while문/10951-A+B-4.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+	int a, b; while (cin>>a) { cin >> b; cout << a + b << "\n"; }
+	return 0;
+}
---
+++ while문/1110-더하기사이클.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	short n, res = 0, count = 0;
+	cin >> n;
+	res = n;
+	while (1)
+	{
+		short a = res / 10, b = res % 10;
+		res = b * 10 + (a + b) % 10;
+		count++;
+		if (res == n)break;
+	}
+	printf("%d", count);
+	return 0;
+}
