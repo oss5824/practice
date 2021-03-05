f83774b1ed4f56a64834e9786ccd31bc8dce94e5
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-03-06 03:11:25 +0900

    .
    .

---
+++ 10952-A+B-5.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+	int a, b; for (;;) { cin >> a >> b; if (a == 0 && b == 0)break; cout << a + b << "\n"; }
+	return 0;
+}
