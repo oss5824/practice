bd9b02cfc6c5de954bf94aaccd13578123a5786f
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-03-16 21:12:44 +0900

    정답
    줄세우기

--- 2252.cpp
+++ 2252.cpp
@@ -1,28 +1,20 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include<iostream>
+#include<vector>
+#include<queue>
 using namespace std;
+int level[32001];
+vector<int>v[32001];
 int main()
 {
-	cin.tie(NULL); ios::sync_with_stdio(false);
-	int n, m, a, b;
-	cin >> n >> m; 
-	vector<int>* v = new vector<int>[n + 1];
-	vector<int>level(n + 1, 0);
-	for (int i = 0; i < m; i++) { cin >> a >> b; v[a].push_back(b); level[b]++; }
-	queue<int>q;
+	cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+	int n, m, a, b; cin >> n >> m; queue<int>q;
+	while (m--) { cin >> a >> b; level[b]++; v[a].push_back(b); }
 	for (int i = 1; i <= n; i++)if (level[i] == 0)q.push(i);
-	for (int i = 1; i <= n; i++)
-	{
-		if (q.empty()) { break; }
-		int cur = q.front(), len = v[cur].size();
+	while (!q.empty()) {
+		int cur = q.front(); q.pop();
 		cout << cur << " ";
-		q.pop();
-		for (int j = 0; j < len; j++)
-		{
-			level[v[cur][j]]--;
-			if (level[v[cur][j]] == 0)q.push(v[cur][j]);
-		}
+		for (int next : v[cur]) { if (--level[next] == 0)q.push(next); }
 	}
 	return 0;
 }
+//cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
