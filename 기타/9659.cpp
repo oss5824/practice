2746fc8dcb34800df1fe36d141f3ee150b6c96fc
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-09-03 09:10:46 +0900

    Add files via upload
    돌게임5

---
+++ 기타/9659.cpp
@@ -0,0 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main(){long long a; scanf("%lld", &a); (a % 2) ? printf("SK") : printf("CY");}
+//cin.tie(NULL);cout.tie(NULL);ios::sync_with_stdio(false);
+
+
