dfb5e927d84fde207b9c321731eace978b8d327e
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-10-04 01:41:57 +0900

    Add files via upload
    선긋기

--- 스위핑/2170.cpp
+++ 스위핑/2170.cpp
@@ -4,24 +4,26 @@
 using namespace std;
 vector<pair<int, int>>arr;
 typedef long long ll;
+bool cmp(pair<int, int>a, pair<int, int>b) {
+	if (a.second == b.second)return a.first < b.first;
+	return a.second < b.second;
+}
 int main()
 {
 	cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
-	int n; cin >> n; arr.assign(n, { 0,0 }); 
+	int n; cin >> n; arr.assign(n, { 0,0 });
 	for (int i = 0; i < n; i++)cin >> arr[i].first >> arr[i].second;
-	stable_sort(arr.begin(), arr.end());
-	int start = arr[0].first, end = arr[0].second; ll result = end - start;
-	for (int i = 1; i < n; i++)
-	{
-		int n_start = arr[i].first, n_end = arr[i].second;
-		if (start <= n_start && n_end <= end)continue;
-		else if (n_start == start) { if (end < n_end) { result += n_end - end; end = n_end; } }
-		else
-		{
-			if (n_start <= end) { result += n_end - end; end = n_end; }
-			else { result += n_end - n_start; start = n_start; }
+	sort(arr.begin(), arr.end(),cmp);
+	pair<int, int>pos = { 2e9,2e9 };
+	int result = 0;
+	for (int i = n - 1; i >= 0; i--) {
+		if (arr[i].second <= pos.first) {
+			pos = arr[i]; result += pos.second - pos.first;
 		}
-	}
-	cout << result << "\n";
+		else if (arr[i].first >= pos.first)continue;
+		else {
+			result += pos.first - arr[i].first; pos.first = arr[i].first;
+		}
+	}cout << result;
 	return 0;
 }
