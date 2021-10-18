ac937da400321b601145ce9930f89a5e7fef8f11
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-10-18 23:26:58 +0900

    Add files via upload
    이진검색트리-트리

--- 트리/5639.cpp
+++ 트리/5639.cpp
@@ -1,33 +1,35 @@
-#include<iostream>
-#include<map>
+#include <iostream>
+#include <map>
 using namespace std;
 map<int, pair<int, int>>m;
 void post(int num)
 {
-	if (m[num].first != 0)post(m[num].first);
-	if (m[num].second != 0)post(m[num].second);
+	if (m[num].first)post(m[num].first);
+	if (m[num].second)post(m[num].second);
 	cout << num << "\n";
 }
 int main()
 {
 	cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
-	int a, root; cin >> a; root = a;
+	int val, a;
+	cin >> val;
 	while (cin >> a)
 	{
-		int val = root;
+		int root = val;
 		while (1)
 		{
-			if (a < val)
+			if (a < root)
 			{
-				if (m[val].first == 0) { m[val].first = a; break; }
-				else val = m[val].first;
+				if (m[root].first != 0)root = m[root].first;
+				else { m[root].first = a; break; }
 			}
 			else
 			{
-				if (m[val].second == 0) { m[val].second = a; break; }
-				else val = m[val].second;
+				if (m[root].second != 0)root = m[root].second;
+				else { m[root].second = a; break; }
 			}
 		}
 	}
-	post(root);
+	post(val);
+	return 0;
 }
